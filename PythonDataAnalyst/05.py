5ad442abb2cae4a29c5eb8a2ceb7bdac451574a1
Rahul-Kumar-Parida <rahulkumarparida56@gmail.com>
2024-06-05 20:49:35 +0530

    addnew

---
+++ PythonDataAnalyst/05.py
@@ -0,0 +1,53 @@
+print("Hello world")
+print("I learn this python for my data analyst carrer")
+print(45)
+print(45.5)
+
+
+# escape sequence (new line)
+print("my name is rahul \n i am 22 year old")  # new line
+print("my name is rahul \t i am 22 year old")  # tab
+
+# This is single line comment Line
+'''This is multiline comment
+i know this is multiline comment'''
+
+
+print("my name is \'Rahul Kumar\'")
+print("my name is \"Rahul Kumar\"")
+print("my name is 'Rahul Kumar'")
+
+print("My name is not rahul", 6, 7, sep="+")
+print("new line")
+
+
+a = 34
+e = 35
+b = "Harry"
+c = True
+d = None
+print(a)
+print(type(a))
+print(b)
+print(type(b))
+print(c)
+print(type(c))
+print(d)
+print(type(d))
+print(a+e)
+
+print(15 % 6)  # remainder
+print(15/6)
+print(15//6)  # int value
+print(2**4)
+print(1+2)
+
+aa = "1"
+bb = "22"
+print(int(aa)+int(bb))
+
+# Taking user input
+
+a = input("Enter Your Name: ")
+b = input("Enter Your Name: ")
+print("Your Name IS:", a+b)
